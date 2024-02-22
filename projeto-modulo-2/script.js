4262c5bf7de8077d376b2f3f92f7a242e86669a4
george-prado <gmeloprado@gmail.com>
2024-02-22 01:06:58 -0300

    Added PVP and PVE

--- projeto-modulo-2/script.js
+++ projeto-modulo-2/script.js
@@ -1,20 +1,21 @@
 const table = [['X', 'X', 'X'], ['X', 'X', 'X'], ['X', 'X', 'X']]
 const p1 = 'X', p2 = 'O'
-let player1 = 'O', player2 = 'X'
-let gameMode = -1
+let player1 = p1, player2 = p2
+let gameMode = 1
 let draw = Math.floor(Math.random() + 1)
 let lastPlayed = ''
+let winner = ''
 
 
-//Escolhe P1 vs. Comp || P1 vs. P2
+//Escolhe P1 vs. Máquina || P1 vs. P2
 function SelectGameMode(){
     const outputLine1 = '::::::::::::::: ESCOLHA COM QUEM DESEJA JOGAR :::::::::::::::\n'
     const outputLine2 = ':::::::::: 1. Jogador vs. Máquina ::::::::::'
     const outputLine3 = ':::::::::: 2. Jogador vs. Jogador ::::::::::'
     
-    while (gameMode = -1) {
+    while (gameMode == -1) {
         let option = Number(prompt(outputLine1 + outputLine2 + outputLine3))
-        if (option != 1 || option != 2){
+        if (option != 1 && option != 2){
             alert('Entre com um modo de jogo válido');
         } else {
             gameMode = option;
@@ -43,12 +44,42 @@ function ShowGameTable(){
     });
 }
 
-function Turn(e){
-    if((draw == 1 && lastPlayed == '') || lastPlayed == player2) {
+//Modo PVP e PVM
+function Turn(e, row, col){
+    if((draw == 1 && lastPlayed == '') || (lastPlayed == player2 && e.innerText == '')) {
         e.innerText = player1;
         lastPlayed = player1;
-    } else if ((draw == 2 && lastPlayed == '') || lastPlayed == player1) {
+
+        table[row][col] = e.innerText;
+    } else if ((draw == 2 && lastPlayed == '') || (lastPlayed == player1 && e.innerText == '')) {
         e.innerText = player2;
         lastPlayed = player2;
+
+        table[row][col] = e.innerText;
+    }
+
+    if(gameMode == 1 && lastPlayed == player1){
+        let random = Math.floor(Math.random() * 9 + 1)
+
+        let cell = document.querySelector(`#cell-${random}`);
+
+        while (cell.innerText != '') {
+            random = Math.floor(Math.random() * 9 + 1)
+            cell = document.querySelector(`#cell-${random}`)
+        }
+        
+        if((draw == 2 && lastPlayed == '') || (lastPlayed == player1 && cell.innerText == '')) {
+            cell.innerText = player2;
+            lastPlayed = player2;
+    
+            let targetRow = Math.floor((random - 1) / 3);
+            let targetCol = (random - 1) % 3;
+
+            table[targetRow][targetCol] = cell.innerText;
+        }
     }
 }
+
+/* function Winner(){
+
+} */
